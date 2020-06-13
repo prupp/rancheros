6fd6ee9845800a461a38dbdafab4f610dcfb52fd
Philipp Rupp <rupp.philipp@gmail.com>
2020-06-13 18:56:22 +0200

    switch to new linuxserver/unifi-controller docker image, fix port conflicts

--- updates/update_unifi.sh
+++ updates/update_unifi.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker stop unifi && docker rm unifi && docker rmi linuxserver/unifi && sudo ros service up unifi
+docker stop unifi && docker rm unifi && docker rmi linuxserver/unifi-controller && sudo ros service up unifi
